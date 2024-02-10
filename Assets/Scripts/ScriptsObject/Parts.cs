818bbf504b5aff1766306f31615ab87e9f0b46f2
Soraku7 <2673140287@qq.com>
2024-02-10 17:23:02 +0800

    ArtResources

--- Assets/Scripts/ScriptsObject/Parts.cs
+++ Assets/Scripts/ScriptsObject/Parts.cs
@@ -6,6 +6,7 @@ namespace ScriptsObject
     public class Parts : ScriptableObject
     {
         public Image image;
+        public float coldTime;
         public  virtual void  Skill()
         {
             
